77346b05cb238a0499b2d58cd2a105016df92693
fengyj <fengyj@mapbar.com>
2016-08-11 16:07:07 +0800

    沉浸式状态栏设置

--- app/src/main/java/com/fengyj/newsapp/main/widget/MainActivity.java
+++ app/src/main/java/com/fengyj/newsapp/main/widget/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fengyj.newsapp.main.widget;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -13,6 +14,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -49,7 +52,13 @@ public class MainActivity extends AppCompatActivity implements MainView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            mToolbar.setTitle(R.string.navigation_news);
+        }
         setSupportActionBar(mToolbar);
+
+
+        mMainPresenter = new MainPresenterImpl(this);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close);
@@ -59,10 +68,10 @@ public class MainActivity extends AppCompatActivity implements MainView {
         mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
         setupDrawerContent(mNavigationView);
 
-        mMainPresenter = new MainPresenterImpl(this);
         setWindowTranslucentStatus();
-        switchTitle(Configs.NEWS_POSITION);
         initFragments();
+
+        mMainPresenter.switchNavigation(R.id.navigation_item_news);
     }
 
     private void setupDrawerContent(NavigationView navigationView) {
@@ -78,15 +87,34 @@ public class MainActivity extends AppCompatActivity implements MainView {
     }
 
     private void setWindowTranslucentStatus() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      /*  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
             localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
                 //将侧边栏顶部延伸至status bar
-                mDrawerLayout.setFitsSystemWindows(true);
+             //   mDrawerLayout.setFitsSystemWindows(true);
                 //将主页面顶部延伸至status bar;虽默认为false,但经测试,DrawerLayout需显示设置
-                mDrawerLayout.setClipToPadding(false);
+              //  mDrawerLayout.setClipToPadding(false);
             }
+        }*/
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){//4.4 全透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                //将侧边栏顶部延伸至status bar
+                 mDrawerLayout.setFitsSystemWindows(true);
+                //将主页面顶部延伸至status bar;虽默认为false,但经测试,DrawerLayout需显示设置
+                 mDrawerLayout.setClipToPadding(false);
+            }
+        }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0 全透明实现
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);//calculateStatusColor(Color.WHITE, (int) alphaValue)
         }
 
     }
@@ -105,6 +133,7 @@ public class MainActivity extends AppCompatActivity implements MainView {
             ft.add(R.id.frame_content, mFragments.get(0));
         }
         ft.commit();
+        mToolbar.setTitle(R.string.navigation_news);
     }
 
     /**
@@ -152,6 +181,9 @@ public class MainActivity extends AppCompatActivity implements MainView {
                 break;
             case Configs.ABOUT_POSITION:
                 mToolbar.setTitle(R.string.navigation_about);
+                break;
+            default:
+                mToolbar.setTitle(R.string.navigation_news);
                 break;
         }
     }
